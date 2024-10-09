60adfd8232fccefc78cd7dd2429e47116f1a41cf
mjyoo <ryuminji0353@gmail.com>
2024-10-09 00:03:08 -0700

    로그인 처리 - HttpSession을 사용하여 세션에 로그인 회원 정보 보관

--- src/main/java/com/mj/myforum/controller/LoginController.java
+++ src/main/java/com/mj/myforum/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.mj.myforum.controller;
 import com.mj.myforum.domain.User;
 import com.mj.myforum.form.LoginForm;
 import com.mj.myforum.service.LoginService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -25,22 +28,34 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute("loginForm") LoginForm form,
+                        BindingResult bindingResult,
+                        HttpServletRequest request) {
+
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
 
-        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
-
         //로그인 검증
+        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
         if (loginUser == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 일치하지 않습니다.");
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
-        log.info("User {} logged in successfully.", form.getLoginId());
-        return "redirect:/";
+        //로그인 성공 처리 (세션이 있으면 세션 반환, 없으면 신규 세션 생성)
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute("loginUser", loginUser);
+        return "posts/postList";
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
 }
