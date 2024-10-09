47d727a95f22ef4e9cb031ef8ffa717c87ba2e1a
mjyoo <ryuminji0353@gmail.com>
2024-10-09 00:53:01 -0700

    로그인 처리 - 미로그인 사용자 특정 웹 페이지에 접근 제한 설정

---
+++ src/main/java/com/mj/myforum/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.mj.myforum.config;
+
+import com.mj.myforum.intercepter.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**") // "/"의 하위 폴더 모두 해당됨
+                .excludePathPatterns("/", "/users/signup", "/login","/logout",
+                        "/css/**", "/*.ico", "/error"); //interceptor 적용 제외
+    }
+
+
+}
