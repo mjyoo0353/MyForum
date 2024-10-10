9fc246b8c88645a7bd306085dc9b62063ce21899
mjyoo <ryuminji0353@gmail.com>
2024-10-10 16:31:10 -0700

    Home view 설계 - 로그인한 사용자만 접속할 수 있도록 설정 로그인, 미로그인 사용자 페이지 구분

---
+++ src/main/java/com/mj/myforum/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.mj.myforum.controller;
+
+import com.mj.myforum.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+public class HomeController {
+
+    //@GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@SessionAttribute(name = "loginUser", required = false)User loginUser,
+                            Model model){
+        //세션에 회원 데이터가 없으면 home
+        if (loginUser == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인된 홈으로 이동
+        model.addAttribute("user", loginUser);
+        return "loginHome";
+    }
+
+
+}
