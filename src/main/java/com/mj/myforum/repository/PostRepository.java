23de19e88f8004103fad8b63e97b7ca14099d06c
mjyoo <ryuminji0353@gmail.com>
2024-10-10 16:20:47 -0700

    Post 기본 repository 및 service 설계

---
+++ src/main/java/com/mj/myforum/repository/PostRepository.java
@@ -0,0 +1,50 @@
+package com.mj.myforum.repository;
+
+import com.mj.myforum.domain.Post;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository{
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public Post save(Post post) {
+        em.persist(post);
+        return post;
+    }
+
+    public Post update(Post post) {
+        return null;
+    }
+
+    public Post delete(Post post) {
+        em.remove(post);
+        return post;
+    }
+
+    public Post findById(Long postId) {
+        return em.createQuery("select p from Post p where p.id = :post_id", Post.class)
+                .setParameter("post_id", postId)
+                .getSingleResult();
+    }
+
+    public List<Post> findAll(){
+        return em.createQuery("select p from Post p", Post.class)
+                .getResultList();
+    }
+
+    // limit 이용해서 한 페이지당 게시글 10개만 출력하기 TODO
+    public List<Post> postsNumPerPage() {
+        return em.createQuery("select p from Post p order by p.id desc", Post.class)
+                .getResultList();
+    }
+
+}
---
+++ src/main/java/com/mj/myforum/service/PostService.java
@@ -0,0 +1,67 @@
+package com.mj.myforum.service;
+
+import com.mj.myforum.domain.Post;
+import com.mj.myforum.domain.User;
+import com.mj.myforum.form.PostForm;
+import com.mj.myforum.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post save(User user, PostForm postForm) {
+        Post post = new Post();
+        post.setTitle(postForm.getTitle());
+        post.setContent(postForm.getContent());
+        post.setWriter(user.getLoginId());
+        post.setCreatedDate(LocalDateTime.now());
+        post.setViews(0L);
+        return postRepository.save(post);
+    }
+
+    public Post delete(Long postId) {
+        Post post = postRepository.findById(postId);
+        return postRepository.delete(post);
+    }
+
+    public Post findById(Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    public List<Post> postList() {
+        return postRepository.findAll();
+    }
+
+    public Post getPost(Long postId) {
+        Post post = postRepository.findById(postId);
+        post.setViews(post.getViews() + 1); //조회수 증가
+        postRepository.save(post); //post 조회수 증가 update
+        return post;
+    }
+
+    public boolean isAccessable(Long postId, User loginUser) {
+        Post post = postRepository.findById(postId);
+        if (post.getWriter().equals(loginUser.getLoginId())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Post updatePost(Long postId, PostForm form) {
+        Post post = postRepository.findById(postId); //수정할 post 불러오기
+        post.setTitle(form.getTitle());
+        post.setContent(form.getContent()); //수정된 제목, 내용으로 업데이트
+        return postRepository.save(post); //post update 완료
+    }
+
+}
